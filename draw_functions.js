e5c3ec455adc89e397567eff4601245c118a2e98
gam0022 <gam0022@gmail.com>
2013-09-11 16:59:49 +0900

    デザインを追加。

--- draw_functions.js
+++ draw_functions.js
@@ -128,6 +128,38 @@ draw_functions['simple_c'] = function (json) {
   };
 };
 
+draw_functions['design_a_summary'] = function (json) {
+
+  var background_image_url = "background_images/design_a.png";
+  var screen_name = json.screen_name;
+  var profile_image_url = json.profile_image_local_url;
+  var name = json.name;
+  var summary = json.summary;
+
+  var img = new Image();
+  img.src = background_image_url;
+  img.onload = function() {
+    context.drawImage(img, 0, 0, 455, 275);
+
+    var profile_image = new Image();
+    profile_image.src = profile_image_url;
+    profile_image.onload = function() {
+      context.drawImage(profile_image, 168, 77, 120, 120);
+
+      context.font = "22px 'MS Gothic'";
+      context.textAlign = "right";
+      context.fillStyle = "rgb(100,100,100)";
+      context.fillText("@" + screen_name, 435, 60);
+
+      context.textAlign = "left";
+      context.font = "12px 'MS Gothic'";
+
+      fillMultilineText(context, name, 150, MW - 150, 90, 15, 5);
+      fillMultilineTextBottom(context, summary, 300, 10, 220, 15, 4);
+    };
+  };
+};
+
 draw_functions['simple_a_summary'] = function (json) {
 
   var background_image_url = "background_images/simple_a.png";
@@ -157,8 +189,7 @@ draw_functions['simple_a_summary'] = function (json) {
 
       fillMultilineText(context, name, MW - 145 - 20, 145 + 5, 90, 15, 3);
 
-      context.fillText("summary:", 145, 120);
-      fillMultilineText(context, summary, 300, 145, 140, 15, 3);
+      fillMultilineText(context, summary, 300, 145, 125, 15, 4);
 
       fillMultilineTextBottom(context, description, 300, 145, 200, 15, 5);
     };
@@ -194,11 +225,45 @@ draw_functions['simple_b_summary'] = function (json) {
 
       fillMultilineText(context, name, MW - 200 - 20, 200 + 5, 90, 15, 3);
 
-      context.fillText("summary:", 75, 160);
-      fillMultilineText(context, summary, MW - 60 - 30, 75, 180, 15, 2);
+      fillMultilineText(context, summary, MW - 60 - 30, 75, 165, 15, 3);
 
       fillMultilineTextBottom(context, description, MW - 60 - 30, 75, 230, 15, 3);
     };
   };
 };
 
+draw_functions['simple_c_summary'] = function (json) {
+
+  var background_image_url = "background_images/simple_c.png";
+  var screen_name = json.screen_name;
+  var profile_image_url = json.profile_image_local_url;
+  var name = json.name;
+  var description = json.description;
+  var summary = json.summary;
+
+  var img = new Image();
+  img.src = background_image_url;
+  img.onload = function() {
+    context.drawImage(img, 0, 0, 455, 275);
+
+    var profile_image = new Image();
+    profile_image.src = profile_image_url;
+    profile_image.onload = function() {
+      context.drawImage(profile_image, 30, 30, 100, 100);
+
+      context.textAlign = "left";
+
+      context.font = "22px 'MS Gothic'";
+      context.fillStyle = "rgb(10,10,10)";
+      context.fillText("@" + screen_name, 150, 60);
+
+      context.font = "12px 'MS Gothic'";
+
+      fillMultilineText(context, name, MW - 150 - 20, 150 + 5, 90, 15, 5);
+
+      fillMultilineText(context, summary, MW - 30 * 2, 30, 165, 15, 3);
+
+      fillMultilineTextBottom(context, description, MW - 30 * 2, 30, 200, 15, 5);
+    };
+  };
+};
